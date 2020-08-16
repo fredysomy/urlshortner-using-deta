69a3340082e04a3a2a53a342a187d94b5be8d872
Fredy Somy <65971072+fredysomy@users.noreply.github.com>
2020-08-17 01:36:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -49,15 +49,6 @@ app.get('/:id',(req,res)=>{
 });
 
 
-app.listen('4345',()=>{
+app.listen('8080',()=>{
     console.log("server on 4345")
 })
-function isURL(str) {
-    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-    return pattern.test(str);
-  }
